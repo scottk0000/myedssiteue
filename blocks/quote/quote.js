3a4416467be32661de4de24ce89056437088cbb9
Scott Kaufmann <scottk0000@gmail.com>
2025-08-11 09:50:25 -0700

    fixing more code

--- blocks/quote/quote.js
+++ blocks/quote/quote.js
@@ -1,10 +1,6 @@
 export default function decorate(block) {
   const [quoteWrapper] = block.children;
-
   const blockquote = document.createElement('blockquote');
-  const quoteText = quoteWrapper.textContent.trim();
-  
-  // Use default text if no content is provided
-  blockquote.textContent = quoteText || 'Think, McFly! Think!';
+  blockquote.textContent = quoteWrapper.textContent.trim() || 'Think, McFly! Think!';
   quoteWrapper.replaceChildren(blockquote);
 }
